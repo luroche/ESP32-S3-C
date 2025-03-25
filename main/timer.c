f278d6f34cf75c26910573455c769611a4fa7053
luroche <luis.roche@hotmail.com>
2025-03-25 16:57:16 +0100

    está funcionando a la perfección con semaforos entre las dos tareas, a 25000 hz

--- main/timer.c
+++ main/timer.c
@@ -8,46 +8,65 @@
 #include "driver/adc.h"
 #include "soc/adc_channel.h"
 #include "driver/timer.h"
+#include "lwip/sockets.h"
+
+
+#define NUM_SAMPLES 200  // Número de muestras por paquete UDP
+#define ADC_CHANNEL ADC1_CHANNEL_0  // Canal del ADC (ajustar según el hardware)
+
+// Buffers de almacenamiento
+static uint16_t buffer1[NUM_SAMPLES];
+static uint16_t buffer2[NUM_SAMPLES];
+volatile uint16_t *active_buffer;
+volatile uint16_t *ready_buffer;
+
+// Semáforo para sincronización
+static SemaphoreHandle_t sem_buffer_ready;
 
 
 #define ADC_WIDTH        ADC_WIDTH_BIT_12   // Resolución de 12 bits para el ADC
 #define ADC1_CHANNEL_0  ADC1_CHANNEL_0     // Canal 0 para ADC1
-#define NUM_SAMPLES      200                  // Número de muestras a tomar por ADC
 
 #define TIMER_DIVIDER    16                // Divisor del temporizador
 #define TIMER_BASE_CLK   80000000          // 80 MHz (frecuencia base del temporizador en ESP32)
 #define TIMER_SCALE      (TIMER_BASE_CLK / TIMER_DIVIDER)  // Frecuencia del temporizador
-#define TIMER_INTERVAL    (TIMER_SCALE / 20) // 25kHz (para 25kHz, 1 / 25kHz = 40us, luego el valor se ajusta en el código)
+#define TIMER_INTERVAL    (TIMER_SCALE / 25000) // 25kHz (para 25kHz, 1 / 25kHz = 40us, luego el valor se ajusta en el código)
 
 static uint32_t adc1_samples[NUM_SAMPLES];  // Buffer para almacenar las muestras de ADC
 static volatile uint32_t interrupts_per_second = 0;  // Contador de interrupciones por segundo
 
-// Cola para enviar las muestras a la tarea de procesamiento
-QueueHandle_t GlobalQueue = 0;
+static volatile int index_lectura = 0;
+
 const char *tag = "main";
 
 // Contador global para el número de ejecuciones de process_adc_samples
-volatile uint32_t process_adc_count = 0;
+volatile uint32_t count_udps = 0;
+
 
 // Función de la interrupción del temporizador
-static _Bool IRAM_ATTR timer_isr(void *param) {
-    static int sample_index = 0;
-    
+static _Bool IRAM_ATTR timer_isr(void *arg) {
+    active_buffer = buffer1;
+    ready_buffer = buffer2;
+
+
     // Leer el ADC (canal 0 de ADC1)
-    adc1_samples[sample_index] = adc1_get_raw(ADC1_CHANNEL_0);
-    if (!xQueueSend(GlobalQueue, &adc1_samples[sample_index], pdMS_TO_TICKS(100))) // si no se pudo enviar, entonces si la cola esta llena retornara este error
-    {
-        ESP_LOGE(tag, "ERROR ENVIANDO");
-        // ESP_LOGE(tag, "Error sending %u to queue", adc1_samples[sample_index]); // Error si no se envia
+    // adc1_samples[sample_index] = adc1_get_raw(ADC1_CHANNEL_0);
+
+    active_buffer[index_lectura] = 5048;
+    index_lectura++;
+
+    if (index_lectura >= NUM_SAMPLES) {
+        // Intercambiar buffers
+        uint16_t *temp = ready_buffer;
+        ready_buffer = active_buffer;
+        active_buffer = temp;
+        // Avisar a la tarea de envío
+        xSemaphoreGive(sem_buffer_ready);
+        
+        // Reiniciar índice
+        index_lectura = 0;
     }
 
-    sample_index++;
-    if (sample_index >= NUM_SAMPLES) {
-        // Se han tomado todas las muestras
-        // Enviar las muestras a la cola
-        // xQueueSendFromISR(adc_samples_queue, adc1_samples, NULL);
-        sample_index = 0;  // Reinicia el índice para tomar nuevas muestras
-    }
 
     // Incrementa el contador de interrupciones por segundo
     interrupts_per_second++;
@@ -58,36 +77,40 @@ static _Bool IRAM_ATTR timer_isr(void *param) {
     return pdTRUE;  // Indicar que la interrupción fue manejada correctamente
 }
 
-// Tarea para procesar las muestras de ADC y contar las ejecuciones
-void process_adc_samples(void *pvParameter) {
-    uint32_t samples[NUM_SAMPLES];
-    uint32_t receivedValue = 0;  // Cambiar a uint32_t
+
+
+
+
+
+
+
+// Función de envío UDP
+void udp_task(void *arg) {
+    // struct sockaddr_in dest_addr;
+    // int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    
+    // dest_addr.sin_addr.s_addr = inet_addr("192.168.1.100"); // IP destino
+    // dest_addr.sin_family = AF_INET;
+    // dest_addr.sin_port = htons(1234); // Puerto destino
 
     while (1) {
-        if (xQueueReceive(GlobalQueue, &receivedValue, pdMS_TO_TICKS(100)) == pdTRUE) {  // Recibir de la cola
-            process_adc_count++;
-            // ESP_LOGI(tag, "Value received %lu from queue", receivedValue);  // Si se recibe
-        } else {
-            ESP_LOGE(tag, "Error receiving value from queue"); // Error si no se recibe
+        // Esperar a que el buffer esté listo
+        if (xSemaphoreTake(sem_buffer_ready, portMAX_DELAY)) {
+            // ESP_LOGI(tag, "Recibe");  // Si se recibe
+            count_udps++;
+            // vTaskDelay(pdMS_TO_TICKS(1000));
+            // sendto(sock, ready_buffer, NUM_SAMPLES * sizeof(uint16_t), 0, 
+            //        (struct sockaddr *)&dest_addr, sizeof(dest_addr));
         }
-        vTaskDelay(pdMS_TO_TICKS(100));  // Asegurarse de no bloquearse indefinidamente
-        // // Recibe todo el tamaño de la cola de una vez
-        // if (xQueueReceive(adc_samples_queue, samples, sizeof(samples)) == pdTRUE) {
-        //     // Incrementar el contador de ejecuciones de la tarea
-        //     process_adc_count++;
-        //
-        //     // No hacemos nada con las muestras, solo incrementamos el contador
-        //     // Si quisieras procesar las muestras, lo harías aquí, por ejemplo, imprimiéndolas.
-        //     // Pero ahora simplemente incrementamos el contador.
-        //     // ESP_LOGI("Process", "Contador de ejecuciones de process_adc_samples: %d", process_adc_count);
-        // }
-
-        // vTaskDelay(pdMS_TO_TICKS(1000));  // Espera de 1 segundo antes de la siguiente iteración
     }
 }
 
+
+
 void app_main(void) {
-    printf("HOLAAAAAAAAAA");
+    printf("Comenzamos 2...");
+    vTaskDelay(pdMS_TO_TICKS(3000));
+    
     // Configuración de ADC
     esp_err_t ret = adc1_config_width(ADC_WIDTH);
     if (ret != ESP_OK) {
@@ -101,12 +124,9 @@ void app_main(void) {
         return;
     }
     
-    // Crear la cola para las muestras del ADC
-    GlobalQueue = xQueueCreate(NUM_SAMPLES, sizeof(uint32_t));
-    if (GlobalQueue == NULL) {
-        ESP_LOGE("Queue", "No se pudo crear la cola para las muestras del ADC");
-        return;
-    }
+    // Crear semáforo
+    sem_buffer_ready = xSemaphoreCreateBinary();
+
 
     // Configuración del temporizador
     timer_config_t config = {
@@ -128,7 +148,7 @@ void app_main(void) {
     timer_start(TIMER_GROUP_0, TIMER_0);
 
     // Crear la tarea para procesar las muestras del ADC
-    xTaskCreate(process_adc_samples, "process_adc_samples", 2048, NULL, 5, NULL);
+    xTaskCreate(udp_task, "udp_task", 2048, NULL, 5, NULL);
 
     // Mensaje inicial
     ESP_LOGI("Main", "Iniciando muestreo ADC en tiempo real...");
@@ -137,8 +157,8 @@ void app_main(void) {
         vTaskDelay(pdMS_TO_TICKS(1000));  // Esperar 1 segundo
 
         // Verificar cuántas veces se ha ejecutado process_adc_samples en el último segundo
-        uint32_t executions_in_last_second = process_adc_count;
-        process_adc_count = 0;  // Resetea el contador de ejecuciones para el siguiente segundo
+        uint32_t executions_in_last_second = count_udps;
+        count_udps = 0;  // Resetea el contador de ejecuciones para el siguiente segundo
         
         printf("El contador de ejecuciones de process_adc_samples en el último segundo es: %lu\n", executions_in_last_second);
 
