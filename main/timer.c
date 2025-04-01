036d79db9611c406551af11606e7c467099d748c
luroche <luis.roche@hotmail.com>
2025-04-01 23:26:25 +0200

    envia 600 datos cada vez, pero reciben 512. envia 125 veces por UDP al segundo

--- main/timer.c
+++ main/timer.c
@@ -32,8 +32,8 @@ static const char *TAG = "UDP_CLIENT";
 #define ADC_CHANNEL ADC1_CHANNEL_0  // Canal del ADC (ajustar según el hardware)
 
 // Buffers de almacenamiento
-static uint16_t buffer1[NUM_SAMPLES];
-static uint16_t buffer2[NUM_SAMPLES];
+static uint16_t buffer1[NUM_SAMPLES*3];
+static uint16_t buffer2[NUM_SAMPLES*3];
 volatile uint16_t *active_buffer = buffer1;
 volatile uint16_t *ready_buffer = buffer2;
 static volatile int index_lectura = 0;
@@ -111,10 +111,12 @@ static _Bool IRAM_ATTR timer_isr(void *arg) {
     // Leer el ADC (canal 0 de ADC1)
     // adc1_samples[sample_index] = adc1_get_raw(ADC1_CHANNEL_0);
 
-    active_buffer[index_lectura] = 5048;
-    index_lectura++;
+    active_buffer[index_lectura] = index_lectura;
+    active_buffer[index_lectura+1] = 5048;
+    active_buffer[index_lectura+2] = 5050;
+    index_lectura = index_lectura + 3;
 
-    if (index_lectura >= NUM_SAMPLES) {
+    if (index_lectura >= (NUM_SAMPLES*3)) {
         // Intercambiar buffers
         uint16_t *temp = ready_buffer;
         ready_buffer = active_buffer;
@@ -142,8 +144,6 @@ static _Bool IRAM_ATTR timer_isr(void *arg) {
 void udp_task(void *arg) {
     struct sockaddr_in server_addr;
     int sock;
-    int vector[] = {10, 20, 30}; // Vector a enviar
-    size_t vector_size = sizeof(vector);
 
     // Crear socket UDP
     sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -163,7 +163,7 @@ void udp_task(void *arg) {
             // ESP_LOGI(tag, "Recibe");  // Si se recibe
             count_udps++;
             // ESP_LOGI(TAG, "Enviando datos a: %s:%d", UDP_SERVER_IP, UDP_PORT);
-            int sent_bytes = sendto(sock, ready_buffer, NUM_SAMPLES * sizeof(uint16_t), 0,
+            int sent_bytes = sendto(sock, ready_buffer, (NUM_SAMPLES*3) * sizeof(uint16_t), 0,
                                     (struct sockaddr *)&server_addr, sizeof(server_addr));
             if (sent_bytes < 0) {
                 ESP_LOGE(TAG, "Error enviando datos: %s", strerror(errno));
